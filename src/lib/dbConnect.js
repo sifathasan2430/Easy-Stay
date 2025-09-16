c349fd8ce8211bcd83e1a46d63180d95a8f17e80
sifat hasan <sifatshasan@gmail.com>
2025-09-16 19:24:54 +0600

    database connect with mongodb

---
+++ src/lib/dbConnect.js
@@ -0,0 +1,19 @@
+
+
+import mongoose from "mongoose"
+const connection={}
+ const  dbConnect=async()=>{
+
+    if (connection.isConnected){
+       return   console.log('mongodb already connected to the database with mongoose and mongodb')
+    }
+    try {
+        const db=await mongoose.connect(process.env.MONGO_URL || "")
+        
+        connection.isConnected=db.connections[0].readyState;
+    } catch (error) {
+         console.log('mongodb is not connected to the  database on',error)
+         process.exit(1)
+    }
+}
+export default dbConnect
