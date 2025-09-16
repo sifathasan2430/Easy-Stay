0cfe6defe4ff1de9145a4a37916c97ff33c0957d
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-09-17 00:37:39 +0600

    [Added]:Reviews

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
 import HeroSection from "./Components/HeroSection";
+import EasyStayReviews from "./Components/EasyStayReviews";
+import  EasyStayPartners  from "./Components/EasyStayPartners";
+
 
 export default function Home() {
   return (
-   <div>
-    {/* Do not Code Here */}
-    <HeroSection></HeroSection>
-   </div>
+    <div>
+      {/* Do not Code Here */}
+      <HeroSection></HeroSection>
+      <EasyStayReviews></EasyStayReviews>
+      <EasyStayPartners></EasyStayPartners>
+      
+      
+    </div>
   );
 }
