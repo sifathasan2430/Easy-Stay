433f42405f203050923a85539e3a9ca745a45ebd
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-10-02 13:50:56 +0600

    [Created]-Profile of user

---
+++ src/components/ConditionalLayout.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { usePathname } from "next/navigation";
+import Header from "@/components/Header/Header";
+import Footer from "../app/Components/Footer";
+import { Toaster } from "sonner";
+
+export default function ConditionalLayout({ children }) {
+  const pathname = usePathname();
+  
+  // Define routes where header and footer should be hidden
+  const hideHeaderFooterRoutes = [
+    '/guest',
+    '/guest/',
+    '/dashboard',
+    '/dashboard/',
+    '/guest/dashboard',
+    '/guest/dashboard/'
+  ];
+  
+  const shouldShowHeaderFooter = !hideHeaderFooterRoutes.some(route => 
+    pathname?.startsWith(route)
+  );
+
+  return (
+    <>
+      {shouldShowHeaderFooter && (
+        <div>
+          <Header />
+        </div>
+      )}
+
+      <main className="min-h-screen">
+        {children}
+        <Toaster />
+      </main>
+
+      {shouldShowHeaderFooter && <Footer />}
+    </>
+  );
+}
